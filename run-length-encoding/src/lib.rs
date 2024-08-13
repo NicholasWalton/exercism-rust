6f7f2ee75298320246ffac16f28b87eb2b28a691
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-13 15:48:31 -0700

    rle: Implement encoding

--- run-length-encoding/src/lib.rs
+++ run-length-encoding/src/lib.rs
@@ -1,6 +1,25 @@
 pub fn encode(source: &str) -> String {
-    // todo!("Return the run-length encoding of {source}.");
-    source.to_string()
+    let mut dest: Vec<String> = vec![];
+    let mut count: i64 = 0;
+    let Some(mut current) = source.chars().next() else { return String::default() };
+    for char in source.chars() {
+        if char == current {
+            count += 1;
+        } else {
+            push_count(&mut dest, count, current);
+            current = char;
+            count = 1;
+        }
+    }
+    push_count(&mut dest, count, current);
+    dest.into_iter().collect()
+}
+
+fn push_count(dest: &mut Vec<String>, count: i64, current: char) {
+    if (count > 1) {
+        dest.push(count.to_string());
+    }
+    dest.push(current.to_string());
 }
 
 pub fn decode(source: &str) -> String {
