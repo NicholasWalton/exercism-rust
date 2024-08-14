bda14353468cfb380105d36185d0e4c3f027b33b
Nicholas Walton <nicholas.walton@gmail.com>
2024-08-14 15:07:49 -0700

    xorcism: Fix Key type perhaps

--- xorcism/src/lib.rs
+++ xorcism/src/lib.rs
@@ -1,13 +1,9 @@
 use std::iter::Cycle;
-use std::ops::{BitAnd, BitXor};
 use std::slice::Iter;
 
 /// A munger which XORs a key with some data
 #[derive(Clone)]
 pub struct Xorcism<'a> {
-    // This field is just to suppress compiler complaints;
-    // feel free to delete it at any point.
-    // _phantom: std::marker::PhantomData<&'a u8>,
     key: Cycle<Iter<'a, u8>>,
 }
 
@@ -15,9 +11,8 @@ impl<'a> Xorcism<'a> {
     /// Create a new Xorcism munger from a key
     ///
     /// Should accept anything which has a cheap conversion to a byte slice.
-    pub fn new(key: &'a [u8]) -> Xorcism<'a>
-    {
-        Xorcism { key: key.into_iter().cycle() }
+    pub fn new<Key: ?Sized + AsRef<[u8]>>(key: &'a Key) -> Xorcism<'a> {
+        Xorcism { key: key.as_ref().iter().cycle() }
     }
 
     /// XOR each byte of the input buffer with a byte from the key.
